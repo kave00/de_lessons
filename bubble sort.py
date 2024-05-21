2489a838433814fb20b06f8d5902958ca4ec3341
kave00 <timahov.n@gmail.com>
2024-05-21 15:07:06 +0200

    Test commit in main

--- bubble sort.py
+++ bubble sort.py
@@ -3,7 +3,7 @@ def bubble_sort (my_array):
     n = len(my_array)
     count = 0
     for i in range(n - 1):
-        for j in range (n - 1):
+        for j in range (n - i -1):
             if my_array[j] > my_array[j+1]:
                 tmp = my_array[j]
                 my_array[j] = my_array[j+1]
